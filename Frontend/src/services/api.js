5e5ee1579c0e55e00d6156530b585b2287e7d77c
DeepDhareru <deepakmon268@gmail.com>
2026-05-14 03:17:03 +0530

    Frontend Deployment Ready

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,17 +1,24 @@
 import axios from "axios";
 
 const API = axios.create({
+
   baseURL:
-    "http://localhost:5000/api",
+    import.meta.env
+      .VITE_API_URL,
+
 });
 
+// TOKEN
 API.interceptors.request.use(
   (req) => {
 
     const token =
-      localStorage.getItem("token");
+      localStorage.getItem(
+        "token"
+      );
 
     if (token) {
+
       req.headers.Authorization =
         token;
     }
